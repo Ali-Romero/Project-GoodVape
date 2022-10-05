dfa04987f811729ff26b059dba57d98001cc636e
Ali-Romero <sherali920825@yandex.ru>
2022-10-05 20:18:36 +0600

    first-commit

--- source/js/app.js
+++ source/js/app.js
@@ -1,3 +1,24 @@
+function initSliderMain() {
+  $('[data-slider-main]').slick({
+    arrows: false,
+    variableWidth: true,
+    slidesToShow: 3,
+    dots: false,
+    infinite: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: true,
+          adaptiveHeight: true,
+        }
+      }
+    ]
+  })
+}
+
 
 function setCurrentYear() {
   $('[data-current-year]').text(new Date().getFullYear())
@@ -20,6 +41,7 @@ function initWow() {
 $(document).ready(function() {
   setCurrentYear()
   initWow()
+  initSliderMain()
 
   $("input").inputmask()
 });
--- source/js/vendor.js
+++ source/js/vendor.js
@@ -3,6 +3,7 @@
 // =include jquery-validation/dist/jquery.validate.min.js
 // =include inputmask/dist/jquery.inputmask.min.js
 // =include wowjs/dist/wow.min.js
+// =include slick-carousel/slick/slick.js
 
 // =include jquery-query-object/jquery.query-object.js
 // =include jquery.cookie/jquery.cookie.js
