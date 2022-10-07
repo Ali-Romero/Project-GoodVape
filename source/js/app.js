f74af2537df9207a223df7d969c962a4aafbad5b
Ali-Romero <sherali920825@yandex.ru>
2022-10-08 04:24:07 +0600

    CooperationSection start

--- source/js/app.js
+++ source/js/app.js
@@ -18,6 +18,7 @@ function initSliderMain() {
     ]
   })
 }
+
 function initDemandToggle() {
   $('.js-arrow-demand').on('click', function () {
     $('.js-arrow-demand').removeClass('active')
@@ -25,6 +26,97 @@ function initDemandToggle() {
   })
 }
 
+function initFinanceToggle() {
+  $('.js-finance-click').on('click', function () {
+    $(this).toggleClass('active-finance')
+    $(this).closest('.js-finance-parent').find('.js-finance-content').slideToggle()
+  })
+}
+
+
+function initRangeSlider() {
+  var $range1 = $(".js-range-slider-1");
+  var $range2 = $(".js-range-slider-2");
+  var $range3 = $(".js-range-slider-3");
+  var $total = $(".js-price")
+  var $totalProfit = $(".js-total-profit")
+    a = 1,
+    b = 80,
+    c = 650,
+    profitNum = 30
+  function sum() {
+
+    var resultRevenue = a * b * c * profitNum
+      $total.text(resultRevenue.toLocaleString());
+
+    var resultProfit = resultRevenue * 26.76 / 100
+      $totalProfit.text(resultProfit.toLocaleString());
+
+  }
+
+  sum()
+
+  $range1.ionRangeSlider({
+    skin: "big",
+    type: "single",
+    min: 1,
+    max: 3,
+    step: 1,
+    grid: true,
+    grid_snap: true,
+    hide_min_max: true,
+    hide_from_to: true,
+    from: 1,
+    prettify_enabled: false,
+    onChange: function (data) {
+      a = data.from;
+      sum();
+    }
+  });
+
+  $range2.ionRangeSlider({
+    skin: "big",
+    min: 75,
+    max: 85,
+    step: 5,
+    grid: true,
+    grid_snap: true,
+    hide_min_max: true,
+    hide_from_to: true,
+    from: 80,
+    onChange: function (data) {
+      b = data.from;
+      sum();
+    }
+  });
+
+  $range3.ionRangeSlider({
+    skin: "big",
+    type: "single",
+    min: 550,
+    max: 800,
+    step: 50,
+    grid: true,
+    grid_snap: true,
+    hide_min_max: true,
+    hide_from_to: true,
+    from: 650,
+    onChange: function (data) {
+      c = data.from;
+      sum();
+    }
+  });
+  $('[data-tab-button]').on('click', function () {
+    var id = $(this).attr('data-tab-target')
+    var $tab = $('[data-tab="' + id + '"')
+
+    $('[data-tab-button]').removeClass('active')
+    $('[data-tab]').removeClass('active')
+
+    $(this).addClass('active')
+    $tab.addClass('active')
+  })
+};
 
 function setCurrentYear() {
   $('[data-current-year]').text(new Date().getFullYear())
@@ -49,6 +141,8 @@ $(document).ready(function() {
   initWow()
   initSliderMain()
   initDemandToggle()
+  initFinanceToggle()
+  initRangeSlider()
 
   $("input").inputmask()
 });
--- source/js/vendor.js
+++ source/js/vendor.js
@@ -4,6 +4,7 @@
 // =include inputmask/dist/jquery.inputmask.min.js
 // =include wowjs/dist/wow.min.js
 // =include slick-carousel/slick/slick.js
+// =include ion-rangeslider/js/ion.rangeSlider.min.js
 
 // =include jquery-query-object/jquery.query-object.js
 // =include jquery.cookie/jquery.cookie.js
